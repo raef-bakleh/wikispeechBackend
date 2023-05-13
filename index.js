5094d16b675cafa715612a8f7113618fd8498fc0
Raef <bakleh.raef@gmail.com>
2023-05-13 21:04:20 +0200

    changes

--- index.js
+++ index.js
@@ -16,9 +16,18 @@ const corsOptions = {
   origin: ["http://localhost:3000", "https://wikispeech-frontend.vercel.app"],
 };
 const fs = require("fs");
-const file = fs.readFileSync("./7F10D6CAC3E27531C4822C587C4B6FED.txt");
+const path = require("path");
+const file = path.join(__dirname, "7F10D6CAC3E27531C4822C587C4B6FED.txt");
+
+app.get(
+  "/.well-known/pki-validation/7F10D6CAC3E27531C4822C587C4B6FED.txt",
+  (req, res) => {
+    res.sendFile(file);
+  }
+);
+
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.get("/speakerCount", getSpeakerCount);
 app.post("/query", runQuery);
 app.get("/generateQuery", generatedQuery);
@@ -26,14 +35,14 @@ app.get("/words", wordsRouter);
 app.get("/allProjects", allProjects);
 app.get("/allVocals", allVocals);
 
-app.get(
-  "/.well-known/pki-validation/7F10D6CAC3E27531C4822C587C4B6FED.txt",
-  (req, res) => {
-    res.sendFile(
-      "/Users/admin/wikispeechBackend/7F10D6CAC3E27531C4822C587C4B6FED.txt"
-    );
-  }
-);
+// app.get(
+//   "/.well-known/pki-validation/7F10D6CAC3E27531C4822C587C4B6FED.txt",
+//   (req, res) => {
+//     res.sendFile(
+//       "/Users/admin/wikispeechBackend/7F10D6CAC3E27531C4822C587C4B6FED.txt"
+//     );
+//   }
+// );
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
