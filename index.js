edfbdefdd71b6466cbe6272c5a7c2df02c0df347
Raef <bakleh.raef@gmail.com>
2023-05-13 22:10:06 +0200

    changes

--- index.js
+++ index.js
@@ -11,20 +11,17 @@ const getSpeakerCount = require("./routes/speakerCount");
 const generatedQuery = require("./routes/dynamicQuery");
 const app = express();
 const PORT = 8080;
+const https = require("https");
 const corsOptions = {
   origin: ["http://localhost:3000", "https://wikispeech-frontend.vercel.app"],
 };
 const fs = require("fs");
-const path = require("path");
-const file = path.join(__dirname, "FFEFC56E8F45D23C62903F0C2CB5CD12.txt");
-
-app.get(
-  "/.well-known/pki-validation/FFEFC56E8F45D23C62903F0C2CB5CD12.txt",
-  (req, res) => {
-    res.sendFile(file);
-  }
-);
-
+const key = fs.readFileSync("private.key");
+const cert = fs.readFileSync("certificate.crt");
+const cred = {
+  key,
+  cert,
+};
 app.use(express.json());
 app.use(cors());
 app.get("/speakerCount", getSpeakerCount);
@@ -34,18 +31,11 @@ app.get("/words", wordsRouter);
 app.get("/allProjects", allProjects);
 app.get("/allVocals", allVocals);
 
-// app.get(
-//   "/.well-known/pki-validation/7F10D6CAC3E27531C4822C587C4B6FED.txt",
-//   (req, res) => {
-//     res.sendFile(
-//       "/Users/admin/wikispeechBackend/7F10D6CAC3E27531C4822C587C4B6FED.txt"
-//     );
-//   }
-// );
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
-
+const httpsServer = https.createServer(cred, app);
+httpsServer.listen(8443);
 sequelize
   .authenticate()
   .then(() =>
