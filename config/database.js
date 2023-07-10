8dd91645ff13f78d5e1a02e5f0c92fba846f54e0
raef-bakleh <bakleh.raef@gmail.com>
2023-07-10 05:43:03 +0200

    changes

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: 5432,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
